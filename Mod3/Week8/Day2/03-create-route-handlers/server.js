40261ec37921174e09a2496f93d771b387e05d99
gootieno <gotieno@geoffreys-MacBook-Pro.local>
2024-04-09 13:49:40 -0700

    static assets

--- Mod3/Week8/Day2/03-create-route-handlers/server.js
+++ Mod3/Week8/Day2/03-create-route-handlers/server.js
@@ -1,4 +1,4 @@
-const http = require('http');
+const http = require("http");
 
 let nextDogId = 1;
 
@@ -18,10 +18,11 @@ const server = http.createServer((req, res) => {
 
   // When the request is finished processing the entire body
 
-  req.on("end", () => { 
+  req.on("end", () => {
     // Parsing the body of the request
     // x-www-form-urlencoded
-    if (reqBody) { //query=mars+rover%21&commit=Search
+    if (reqBody) {
+      //query=mars+rover%21&commit=Search
       req.body = reqBody
         .split("&") //[query=mars+rover%21,commit=Search]
         .map((keyValuePair) => keyValuePair.split("=")) //[[query,mars+rover%21],[commit,Search]]
@@ -31,7 +32,7 @@ const server = http.createServer((req, res) => {
           acc[key] = value;
           return acc;
         }, {});
-        /*
+      /*
           {
             query: mars rover!,
             commit: Search
@@ -43,17 +44,63 @@ const server = http.createServer((req, res) => {
     // Do not edit above this line
 
     // define route handlers here
+    if (req.method === "GET" && req.url === "/") {
+      const responseBody = "Dog Club";
 
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/plain");
+      return res.end(responseBody);
+    }
 
+    if (req.method === "GET" && req.url === "/dogs") {
+      const responseBody = "Dogs Index";
+
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/plain");
+      return res.end(responseBody);
+    }
+
+    if (req.method === "GET" && req.url.startsWith("/dogs/")) {
+      console.log("req url ", req.url);
+      const urlParts = req.url.split("/");
+      console.log("url part ", urlParts);
+
+      if (urlParts.length === 3) {
+        const dogId = urlParts[2];
+        if (dogId === "new") {
+          const responseBody = "Dog create form page";
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "text/plain");
+          return res.end(responseBody);
+        } else {
+          const responseBody = `Dog details for dogId: ${dogId}`;
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "text/plain");
+          return res.end(responseBody);
+        }
+      }
+    }
+
+    if (req.method === "POST" && req.url.startsWith("/dogs/")) {
+      console.log("req url ", req.url);
+      const urlParts = req.url.split("/");
+      console.log("url part ", urlParts);
+      if (urlParts.length === 3) {
+        const dogId = urlParts[2];
+        res.statusCode = 302;
+        res.setHeader("Location", `/dogs/${dogId}`);
+        return res.end();
+      }
+    }
 
     // Do not edit below this line
     // Return a 404 response when there is no matching route handler
     res.statusCode = 404;
-    res.setHeader('Content-Type', 'text/plain');
-    return res.end('No matching route handler found for this endpoint');
+    res.setHeader("Content-Type", "text/plain");
+    return res.end("No matching route handler found for this endpoint");
   });
 });
 
 const port = 5000;
 
-server.listen(port, () => console.log('Server is listening on port', port));
+server.listen(port, () => console.log("Server is listening on port", port));
--- Mod3/Week8/Day2/04-serve-static-assets/server.js
+++ Mod3/Week8/Day2/04-serve-static-assets/server.js
@@ -1,10 +1,17 @@
-const http = require('http');
+const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   // Your code here
+
+  if (req.method === "GET" && req.url === "/") {
+    const responseBody = fs.readFileSync("./index.html", "utf-8");
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    return res.end(responseBody);
+  }
 });
 
 const port = 5000;
 
-server.listen(port, () => console.log('Server is listening on port', port));
+server.listen(port, () => console.log("Server is listening on port", port));
