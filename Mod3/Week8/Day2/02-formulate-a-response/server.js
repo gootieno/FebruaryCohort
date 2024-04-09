7149c72365521d3b22240eea0f15b97e8ebc1063
gootieno <gotieno@geoffreys-MacBook-Pro.local>
2024-04-09 10:04:20 -0700

    server boiler plate

--- Mod3/Week8/Day2/02-formulate-a-response/server.js
+++ Mod3/Week8/Day2/02-formulate-a-response/server.js
@@ -1,1 +1,10 @@
 // Your code here
+const http = require('http')
+
+const server = http.createServer((req, res) =>  {
+    //server code here
+})
+
+const port = 5000;
+
+server.listen(port, () => console.log('Server is listening on port', port));
