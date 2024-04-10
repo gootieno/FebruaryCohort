42b48c4290b90ec1f8fb34ce01cdd0e5c8221ebe
gootieno <gotieno@geoffreys-MacBook-Pro.local>
2024-04-09 17:03:10 -0700

    all solutions

--- Mod3/Week8/Day2/02-formulate-a-response/server.js
+++ Mod3/Week8/Day2/02-formulate-a-response/server.js
@@ -5,25 +5,6 @@ const server = http.createServer((req, res) => {
   //server code here
 
   console.log("request  object ", req);
-  if (req.method === "GET" && req.url === "/") {
-    const responseBody = `<!DOCTYPE html>
-        <html lang="en">
-        <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>Hello World!</title>
-        </head>
-        <body>
-          <h1>Hello there!</h1>
-        </body>
-        </html>`;
-
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/html");
-    //   res.write(responseBody)
-    //   res.end("this is the end")
-    return res.end(responseBody);
-  }
 
   if (req.method === "GET" && req.url === "/") {
     const responseBody = `<!DOCTYPE html>
@@ -44,8 +25,6 @@ const server = http.createServer((req, res) => {
     //   res.end("this is the end")
     return res.end(responseBody);
   }
-
-
 });
 
 const port = 5000;
